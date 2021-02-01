025b286f48e757fe8be6571c460128d7c1285fd5
kgund <kgund@digite.com>
2021-02-01 10:52:24 +0530

    Passing test for testRefuelCarWithExcessFuel

--- src/main/java/PetrolPump.java
+++ src/main/java/PetrolPump.java
@@ -3,6 +3,11 @@ import com.digite.kata.Vehicle;
 public class PetrolPump {
     public void refuelVehicle(Vehicle vehicle, int fuelquantity) {
         int remainingFuel = vehicle.getRemainingFuel();
-        vehicle.setRemainingFuel(remainingFuel + fuelquantity);
+        int maxCapacity = vehicle.getMaxFuel();
+        int totalFuel = remainingFuel + fuelquantity;
+        if (totalFuel > maxCapacity){
+            totalFuel = maxCapacity;
+        }
+        vehicle.setRemainingFuel(totalFuel);
     }
 }
